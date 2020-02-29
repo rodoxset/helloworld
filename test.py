48d57bd8960e0c33d9b568e00934684c628cac2b
Rodolfo Sousa <rsetsousa@gmail.com>
2020-02-29 14:39:05 -0300

    Alterado o test

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "CampusParty")
+        self.assertRegex(result.data.decode(), "PósDevopsimpacta")
 
 
 if __name__ == "__main__":
